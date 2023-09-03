3925cf699ac8c3d3f8e8b91c6b81f09faa95e939
Angelo Magno <angelompr.edu@gmail.com>
2023-09-03 20:20:03 -0300

    Update: indent

--- e-commerce/Create_Ecommerce.sql
+++ e-commerce/Create_Ecommerce.sql
@@ -4,29 +4,29 @@ USE e_commerce;
 
 
 -- Cria tabela cliente
-CREATE TABLE clients(
+CREATE TABLE clients (
 	idClient INT AUTO_INCREMENT,
-    Fname VARCHAR(10),
+	Fname VARCHAR(10),
     Minit CHAR(3),
-    Lname VARCHAR(20),
-    CPF CHAR(11) NOT NULL,
-    Address VARCHAR(30),
-    Phone VARCHAR(15),
-    CONSTRAINT unique_cpf_client UNIQUE(CPF),
-    PRIMARY KEY(idClient)
+	Lname VARCHAR(20),
+	CPF CHAR(11) NOT NULL,
+	Address VARCHAR(30),
+	Phone VARCHAR(15),
+	CONSTRAINT unique_cpf_client UNIQUE (CPF),
+	PRIMARY KEY (idClient)
 );
 
 
 -- Cria tabela produto
-CREATE TABLE product(
+CREATE TABLE product (
 	idProduct INT AUTO_INCREMENT,
-    Pname VARCHAR(10) NOT NULL,
-    Classification_kids BOOL DEFAULT FALSE,
-    Category ENUM('Alimentos', 'Brinquedos', 'Eletrônicos', 'Móveis', 'Vestimenta') NOT NULL,
-    Price FLOAT NOT NULL,
-    Rating FLOAT DEFAULT 0,
-    Size VARCHAR(10),
-    PRIMARY KEY(idProduct)
+	Pname VARCHAR(10) NOT NULL,
+	Classification_kids BOOL DEFAULT FALSE,
+	Category ENUM('Alimentos', 'Brinquedos', 'Eletrônicos', 'Móveis', 'Vestimenta') NOT NULL,
+	Price FLOAT NOT NULL,
+	Rating FLOAT DEFAULT 0,
+	Size VARCHAR(10),
+	PRIMARY KEY (idProduct)
 );
 
 
@@ -64,16 +64,16 @@ CREATE TABLE Deliver (
 -- Cria tabela de pedidos
 CREATE TABLE orders(
 	idOrder INT AUTO_INCREMENT,
-    fk_idClient INT,
+	fk_idClient INT,
 	fk_idDeliver INT NOT NULL,
 	fk_idPaymentType INT NOT NULL,
 	fk_idCard INT,
-    TotalValue FLOAT NOT NULL,
-    `Date` DATE NOT NULL,
-    OrderStatus ENUM('Cancelado', 'Confirmado', 'Em processamento') DEFAULT 'Em processamento',
-    OrderDescription VARCHAR(255),
+	TotalValue FLOAT NOT NULL,
+	`Date` DATE NOT NULL,
+	OrderStatus ENUM('Cancelado', 'Confirmado', 'Em processamento') DEFAULT 'Em processamento',
+	OrderDescription VARCHAR(255),
 	PRIMARY KEY(idOrder, fk_idClient, fk_idDeliver),
-    CONSTRAINT fk_orders_client FOREIGN KEY(fk_idClient) REFERENCES clients(idClient),
+	CONSTRAINT fk_orders_client FOREIGN KEY(fk_idClient) REFERENCES clients(idClient),
 	CONSTRAINT fk_Orders_Deliver FOREIGN KEY (fk_idDeliver) REFERENCES Deliver(idDeliver),
 	CONSTRAINT fk_Orders_Card FOREIGN KEY (fk_idCard) REFERENCES Cards(idCard),
 	CONSTRAINT fk_Orders_PaymentType FOREIGN KEY (fk_idPaymentType) REFERENCES paymentTypes(idPayment)
@@ -82,20 +82,20 @@ CREATE TABLE orders(
 
 -- Cria associação de pedido e produto
 CREATE TABLE orders_Product (
-  fk_idProduct INT NOT NULL,
-  fk_idOrder INT NOT NULL,
-  Quantity INT NULL DEFAULT 1,
-  PRIMARY KEY (fk_idProduct, fk_idOrder),
-  CONSTRAINT fk_Product_has_Order_Pd FOREIGN KEY (fk_idProduct) REFERENCES Product (idProduct),
-  CONSTRAINT fk_Product_has_Order_Od FOREIGN KEY (fk_idOrder) REFERENCES Orders (idOrder)
+	fk_idProduct INT NOT NULL,
+	fk_idOrder INT NOT NULL,
+	Quantity INT NULL DEFAULT 1,
+	PRIMARY KEY (fk_idProduct, fk_idOrder),
+	CONSTRAINT fk_Product_has_Order_Pd FOREIGN KEY (fk_idProduct) REFERENCES Product (idProduct),
+	CONSTRAINT fk_Product_has_Order_Od FOREIGN KEY (fk_idOrder) REFERENCES Orders (idOrder)
 );
 
 
 -- Cria tabela de estoque
 CREATE TABLE stock(
 	idStock INT AUTO_INCREMENT,
-    StockLocation VARCHAR(255) NOT NULL,
-    PRIMARY KEY(idStock)
+	StockLocation VARCHAR(255) NOT NULL,
+	PRIMARY KEY(idStock)
 );
 
 -- Cria associação de stoque e produto
@@ -112,21 +112,21 @@ CREATE TABLE product_stock(
 -- Cria tabela de fornecedor
 CREATE TABLE supplier(
 	idSupplier INT AUTO_INCREMENT,
-    SocialName VARCHAR(255) NOT NULL,
-    CNPJ CHAR(15) NOT NULL,
-    Contact CHAR(11) NOT NULL,
-    PRIMARY KEY(idSupplier),
-    CONSTRAINT unique_supplier UNIQUE(CNPJ)
+	SocialName VARCHAR(255) NOT NULL,
+	CNPJ CHAR(15) NOT NULL,
+	Contact CHAR(11) NOT NULL,
+	PRIMARY KEY(idSupplier),
+	CONSTRAINT unique_supplier UNIQUE(CNPJ)
 ); 
 
 -- Cria associação de fornecedor e produto
 CREATE TABLE supplier_Product (
-  fk_idSupplier INT NOT NULL ,
-  fk_idProduct INT NOT NULL,
-  Quantity INT NOT NULL,
-  PRIMARY KEY (fk_idSupplier, fk_idProduct),
-  CONSTRAINT fk_Supplier_has_Product_Sp FOREIGN KEY (fk_idSupplier)REFERENCES Supplier(idSupplier),
-  CONSTRAINT fk_Supplier_has_Product_Pd FOREIGN KEY (fk_idProduct) REFERENCES Product (idProduct)
+	fk_idSupplier INT NOT NULL ,
+	fk_idProduct INT NOT NULL,
+	Quantity INT NOT NULL,
+	PRIMARY KEY (fk_idSupplier, fk_idProduct),
+	CONSTRAINT fk_Supplier_has_Product_Sp FOREIGN KEY (fk_idSupplier)REFERENCES Supplier(idSupplier),
+	CONSTRAINT fk_Supplier_has_Product_Pd FOREIGN KEY (fk_idProduct) REFERENCES Product (idProduct)
 );
 
 
